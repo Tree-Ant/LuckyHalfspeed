6b957a960ad508fd91c09522d6d9f60f293d7f3a
Tree Ant <stokes.finance@gmail.com>
2020-01-17 16:38:49 -0600

    first commit

---
+++ public/app.js
@@ -0,0 +1,99 @@
+$("#new-entry").on("click", function () {
+  $.ajax({
+    type: "POST",
+    url: "/projects/newProject",
+    dataType: "json",
+    data: {
+      title: $("#titleINPUT").val(),
+      date: $("#dateINPUT").val(),
+      idea: $("#ideaINPUT").val(),
+      setting: $("#settingINPUT").val(),
+      location: $("#locationINPUT").val(),
+      author: $("#authorINPUT").val(),
+      imgLink: $("#imgLinkINPUT").val(),
+      comments: [""],
+      created: Date.now()
+    }
+  })
+    .then(function (data) {
+      console.log(data);
+      getEntries();
+      $("#titleINPUT").val(""),
+        $("#dateINPUT").val(""),
+        $("#ideaINPUT").val(""),
+        $("#settingINPUT").val(""),
+        $("#locationINPUT").val(""),
+        $("#authorINPUT").val(""),
+        $("#imgLinkINPUT").val("")
+    }
+    );
+  return false;
+});
+
+// ==============POST ALL PROJECTS==============
+function getEntries() {
+  $("#entries").empty();
+  $.getJSON("/entries", function (data) {
+    console.log(data[1].comments.length);
+    for (var i = 0; i < data.length; i++) {
+      $("#entries").prepend(
+        '<div class="col-xl-4 col-lg-6 col-md-6 col-sm-12">' +
+        '<div class="card">' +
+        '<div class="card-header">' +
+        '<h3 class="card-title titleSPOT">' + data[i].title + '</h3></div>' +
+        '<div class="card-body">' +
+        '<p class="card-text floatR">Date: <span class="dateSPOT">' + data[i].date + '</span></p>' +
+        '<h5 class="card-text">Idea:</h5><br>' +
+        '<p class="card-text ideaSPOT">' + data[i].idea + '</p>' +
+        '<h5 class="card-text settingSPOT">Setting:</h5>' +
+        '<p class="card-text">' + data[i].setting + '</p>' +
+        '<h5 class="card-text">Location:</h5>' +
+        '<p class="card-text locationSPOT">' + data[i].location + '</p>' +
+        '<p class="card-text author floatR"><img src="/assets/images/icon.svg" alt="icon"><span class="authorSPOT ">' + " " +  data[i].author + '</span></p>' +
+        '<img class="imgLinkSpot" src="' + data[i].imgLink + '" alt="photo">' + '<br><br>' +
+        '<button class="btn btn-danger floatR deleter" data-id="' + data[i]._id + '">DELETE</button>' +
+        '<button class="btn btn-secondary"  href="/comments/" data-id="' + data[i]._id + '">Comments</button><br>' +
+        // ============COMMENTS===========
+        '<div id="comments"><br><h5>Comments</h5></div><br>' +         
+        '<div class="input-group"><input type="text" class="form-control" id="commentINPUT" aria-label="Text input with dropdown button"></div><br>' +
+        '<div class="input-group"><select class="custom-select" id="commentName" aria-label="Example select with button addon">' +
+        '<option selected>Choose...</option><option value="Trent: <br>">Trent</option><option value="Rob: <br>">Rob</option>' +
+        '<option value="Thomas: <br>">Thomas</option><option value="Bob: <br>">Bob</option><option value="Jess: <br>">Jess</option></select>' +
+        '<div class="input-group-append"><button class="btn btn-outline-secondary commenter" data-id="' + data[i]._id + '" type="button">POST</button></div></div>' +
+        '</div></div></div></div></div>')
+        console.log("index: " + i);
+      for (var j = 1; j < data[i].comments.length; j++) {
+        console.log("whoops" + j);
+        $("#comments").append(
+          '<br><p class="singleComment">' + data[i].comments[j] + '</p>')}
+    };
+  });
+}
+
+// ==============DELETE==============
+$(document).on("click", ".deleter", function () {
+  var thisId = $(this).attr("data-id");
+  $.ajax({
+    type: "DELETE",
+    url: "/delete/" + thisId
+  }); 
+  getEntries();
+});
+
+// ==============COMMENT==============
+$(document).on("click", ".commenter", function () {  
+  var thisId = $(this).attr("data-id");
+  var thisComment = $(this).val("");
+  $.ajax({
+    type: "POST",
+    url: "/newComment/" + thisId,
+    dataType: "json",
+    data: 
+      { 
+        comments: $('#commentName').val() + $('#commentINPUT').val()
+      }    
+  }); 
+  getEntries();
+});
+
+getEntries();
---
+++ server.js
@@ -0,0 +1,69 @@
+const express = require("express");
+const mongojs = require("mongojs");
+const logger = require("morgan");
+const path = require("path");
+
+const app = express();
+
+
+app.use(logger("dev"));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static("public"));
+
+const databaseUrl = "project";
+const collections = ["entries"];
+
+const db = mongojs(databaseUrl, collections);
+
+db.on("error", error => {
+    console.log("Database Error:", error);
+  });
+
+app.get("/entries", (req, res) => {
+  db.entries.find({}, (err, found) => {
+    if (err) {
+      console.log(err);
+    } else {
+      res.json(found);
+    }
+  });
+});
+
+app.post("/projects/newProject", ({ body }, res) => {
+  const newProject = body;
+  db.entries.save(newProject, (err, saved) => {
+    if (err) {
+      console.log(err);
+    } else {
+      res.json(saved);
+    }
+  });    
+});
+
+app.post("/newComment/:id", (req, res) => {  
+    console.log(req.body);
+  db.entries.updateOne({_id: mongojs.ObjectId(req.params.id)}, {$push: {comments : req.body.comments}})
+  
+});
+
+// =============DELETE========================
+app.delete("/delete/:id", (req, res) => {    
+  db.entries.remove({_id: mongojs.ObjectId(req.params.id)})
+});
+  
+// =================html Paths================ 
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
+app.get('/projects', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/projects.html'));
+});
+
+// =================Listener================
+app.listen(8000, () => {
+  console.log("App running on port 8000!");
+});
